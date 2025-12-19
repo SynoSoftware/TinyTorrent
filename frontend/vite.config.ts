57838b0e88bb715a817485f4bb4c350fe76d9847
SynoSoftware <mariussp@proton.me>
2025-12-19 14:17:35 -0300

    changed frontend to produce a single javascript

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -21,19 +21,12 @@ export default defineConfig({
         },
     },
     build: {
-        chunkSizeWarningLimit: 900,
+        chunkSizeWarningLimit: 3000,
+        cssCodeSplit: false,
         rollupOptions: {
             output: {
-                manualChunks(id) {
-                    if (!id.endsWith(".js")) return;
-                    if (id.includes("node_modules")) {
-                        if (id.includes("@heroui")) return "heroui";
-                        if (id.includes("lucide-react")) return "icons";
-                        if (id.includes("framer-motion")) return "motion";
-                        if (id.includes("@dnd-kit")) return "drag-kit";
-                        if (id.includes("@tanstack")) return "table";
-                        return "vendor";
-                    }
+                manualChunks() {
+                    return "bundle";
                 },
             },
         },
